775b76eb26e554a15ae7ad55ad19870a2a993dc2
Alexey Miheev <almiheev88@gmail.com>
2020-02-10 14:34:48 +0300

    Add e2e.test

--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -3,9 +3,11 @@ import renderer from "react-test-renderer";
 import WelcomeScreen from "./welcome-screen.jsx";
 import {Settings} from '../../utils.js';
 
+const playButtonHandler = () => {};
+
 it(`Render App`, () => {
   const tree = renderer
-    .create(<WelcomeScreen errorsCount={Settings.ERRORS_COUNT} />)
+    .create(<WelcomeScreen errorsCount={Settings.ERRORS_COUNT} onPlayButtonClick={playButtonHandler} />)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
