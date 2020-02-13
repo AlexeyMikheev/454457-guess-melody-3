ad3d4e54b4a5fb2add1060a0a2618398720b90a5
Alexey Miheev <almiheev88@gmail.com>
2020-02-13 09:21:37 +0300

    add components

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import ArtistQuestion from '../artist-question/artist-question.jsx';
+import GenreQuestion from '../genre-question/genre-question.jsx';
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
-const playButtonHandler = () => {};
+const playButtonHandler = () => { };
 
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
   const {errorsCount} = props;
   return (
-    <WelcomeScreen errorsCount={errorsCount} onPlayButtonClick={playButtonHandler}/>
+    <React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <WelcomeScreen errorsCount={errorsCount} onPlayButtonClick={playButtonHandler} />
+          </Route>
+          <Route exact path="/dev-artist">
+            <ArtistQuestion />
+          </Route>
+          <Route exact path="/dev-genre">
+            <GenreQuestion />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
   );
 };
 
